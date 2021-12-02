b1f010201bee46721ed1e749dd1c9f3551b4bd7f
Seong Wook Lim <40172001+SeongWk@users.noreply.github.com>
2021-12-02 15:31:59 +0900

    Add files via upload

---
+++ Navigation/Navigation/EditViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EditViewController.swift
+//  Navigation
+//
+//  Created by seong_wook_lim on 2021/12/01.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    @IBOutlet var lblWay: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnClear(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
